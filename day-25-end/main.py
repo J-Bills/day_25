c1c2220782ad3435aee47131773bf5234edd25ee
J-Bills <billsjamal@gmail.com>
2024-05-06 16:03:08 -0700

    Use squirrel census data with pandas

--- day-25-end/main.py
+++ day-25-end/main.py
@@ -1,83 +1,23 @@
+"""Goal"""
+#Create a dataframe that sums the squirrel data fomr central park based on fur color
+#Create a csv using that data frame
 
-#Using just file methods
-with open("weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+import pandas as pd
 
+data_dict = dict()
+data = pd.read_csv('./2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
 
-#Using csv library
-import csv
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+data_dict = {
+    'Fur_Color' : ['Gray', 'Cinnamon', 'Black'],
+    'Count': [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+    }
 
-
-# Using the pandas library
-import pandas
-
-data = pandas.read_csv("weather_data.csv")
-print(type(data))
-print(type(data["temp"]))
-
-data_dict = data.to_dict()
 print(data_dict)
 
-temp_list = data["temp"].to_list()
-print(len(temp_list))
-
-print(data["temp"].mean())
-print(data["temp"].max())
-
-#Get Data in Columns
-print(data["condition"])
-print(data.condition)
-
-# Get Data in Row
-print(data[data.day == "Monday"])
-print(data[data.temp == data.temp.max()])
-
-# Get Row data value
-monday = data[data.day == "Monday"]
-monday_temp = int(monday.temp)
-monday_temp_F = monday_temp * 9/5 + 32
-print(monday_temp_F)
-
-# Create a dataframe from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-
-
-#Central Park Squirrel Data Analysis
-import pandas
-
-data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
-red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
-print(grey_squirrels_count)
-print(red_squirrels_count)
-print(black_squirrels_count)
-
-data_dict = {
-    "Fur Color": ["Gray", "Cinnamon", "Black"],
-    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
-}
-
-df = pandas.DataFrame(data_dict)
-df.to_csv("squirrel_count.csv")
-
-
-
-
-
-
+custom_frame = pd.DataFrame(data_dict)
+custom_frame.to_csv("squirrel_data.csv")
 
